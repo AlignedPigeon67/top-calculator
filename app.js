ae2df46cb2763e5310429d47210623ebf2137f0a
Tom Hine <tomhine792@gmail.com>
2020-12-12 14:55:13 +0000

    same as...

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const keyNums = document.querySelectorAll(".key-num");
+const key2 = document.querySelector('#key-2');
+
+keyNums.forEach(e => {
+    e.addEventListener("click", () => {
+        console.log(e.getAttribute("id"));
+    })
+})
+
 function add(num1, num2) {
     return num1 + num2;
 }
