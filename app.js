74865cda8978d65d88a4b735fe38509f2e5ea775
tomhine <tomhine792@user.github.com>
2020-12-12 15:32:19 +0000

    Implement screen display and clear and memory clear

--- app.js
+++ app.js
@@ -1,11 +1,68 @@
-const keyNums = document.querySelectorAll(".key-num");
-const key2 = document.querySelector('#key-2');
+const keyNums = document.querySelectorAll('.key-num');
+const keyOperators = document.querySelectorAll('.key-operator');
+const screen = document.querySelector('#screen');
+
+let screenText = '';
 
 keyNums.forEach(e => {
-    e.addEventListener("click", () => {
-        console.log(e.getAttribute("id"));
-    })
-})
+    e.addEventListener('click', () => {
+        switch (e.getAttribute('id')) {
+            case 'key-1':
+                populateDisplay('1');
+                break;
+            case 'key-2':
+                populateDisplay('2');
+                break;
+            case 'key-3':
+                populateDisplay('3');
+                break;
+            case 'key-4':
+                populateDisplay('4');
+                break;
+            case 'key-5':
+                populateDisplay('5');
+                break;
+            case 'key-6':
+                populateDisplay('6');
+                break;
+            case 'key-7':
+                populateDisplay('7');
+                break;
+            case 'key-8':
+                populateDisplay('8');
+                break;
+            case 'key-9':
+                populateDisplay('9');
+                break;
+        }
+    });
+});
+
+keyOperators.forEach(e => {
+    e.addEventListener('click', () => {
+        switch (e.getAttribute('id')) {
+            case 'key-add':
+                populateDisplay('+');
+                break;
+            case 'key-subtract':
+                populateDisplay('-');
+                break;
+            case 'key-multiply':
+                populateDisplay('*');
+                break;
+            case 'key-divide':
+                populateDisplay('/');
+                break;
+            case 'key-equals':
+                populateDisplay('=');
+                break;
+            case 'key-clear':
+                clearScreen();
+                clearMemory();
+                break;
+        }
+    });
+});
 
 function add(num1, num2) {
     return num1 + num2;
@@ -36,6 +93,15 @@ function operate(num1, operator, num2) {
     }
 }
 
-function populateDisplay() {
+function clearScreen() {
+    screen.innerHTML = '';
+}
 
+function clearMemory() {
+    screenText = '';
+}
+
+function populateDisplay(input) {
+    screenText += ' ' + input;
+    screen.innerHTML = screenText;
 }
